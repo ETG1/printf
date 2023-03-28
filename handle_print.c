299b8ba885576015875a9b163108b87474e10376
kdk-creator <kelvindkelvin2019@gmail.com>
2023-03-28 11:35:47 -0700

    alx task team work update

--- handle_print.c
+++ handle_print.c
@@ -5,12 +5,13 @@
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
+ * @flags: to Calculates active flags
+ * @width: to get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
  */
+
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
